986becb00577321eb7ed6035931ed7ae641b9917
akshit109 <jaiswalakshit4@gmail.com>
2025-12-24 19:07:18 +0530

    hash fix

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,12 @@ import { AuthProvider } from './context/AuthContext';
 import './index.css';
 import './App.css';
 
+import { HashRouter } from 'react-router-dom';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
     <AuthProvider>
       <App />
     </AuthProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
